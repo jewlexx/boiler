0200a6162cddc92b5e59de22c6fdb93bc94dea8a
Juliette Cordor <grassojames5@gmail.com>
2022-05-14 21:59:30 +1000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal::{Clear, ClearType, SetTitle},
+    tty::IsTty,
+};
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = io::stdout();
+
+    if !stdout.is_tty() {
+        panic!("is not tty")
+    }
+
+    execute!(
+        stdout,
+        Clear(ClearType::All),
+        SetTitle("Boiler"),
+        ResetColor
+    )?;
+
+    Ok(())
 }
